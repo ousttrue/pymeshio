77fbf72a5833ea1036425262bd7e29c5d3e6ce73
ousttrue <ousttrue@gmail.com>
2011-05-30 23:22:20 +0900

    update 1.8.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
         name='pymeshio',
-        version='1.8.4',
+        version='1.8.5',
         description='pure python 3d model io library',
         keywords=[],
         author='ousttrue',
