f5c2de3f886db8543cb29ad0a58915baea264e6c
ousttrue <ousttrue@gmail.com>
2012-01-17 10:32:39 +0900

    refactoring

--- setup.py
+++ setup.py
@@ -1,21 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from setuptools import setup
+from setuptools import setup, find_packages
 import sys
 import os
 import shutil
 
-name='pymeshio'
-version='2.4.2'
-short_description='3d model io library for mqo, pmd, pmx, vmd and vpd'
-long_description=open('README.rst').read()
-
-classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: zlib/libpng License',
-        'Topic :: Multimedia :: Graphics :: 3D Modeling',
-        ]
 
 # copy pymeshio dir for blender26 plugin
 PYMESHIO_DIR_IN_BLENDER26='blender26-meshio/pymeshio'
@@ -26,26 +16,21 @@ shutil.copytree('pymeshio', PYMESHIO_DIR_IN_BLENDER26)
 
 
 setup(
-        name=name,
-        version=version,
-        description=short_description,
-        long_description=long_description,
-        classifiers=classifiers,
+        name='pymeshio',
+        version='2.4.2',
+        description='3d model io library for mqo, pmd, pmx, vmd and vpd',
+        long_description=open('README.rst').read(),
+        classifiers=[
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: zlib/libpng License',
+            'Topic :: Multimedia :: Graphics :: 3D Modeling',
+            ],
         keywords=['mqo', 'pmd', 'pmx', 'vmd', 'vpd', 'mmd', 'blender'],
         author='ousttrue',
         author_email='ousttrue@gmail.com',
-        url='http://meshio.sourceforge.jp/',
+        url='http://pypi.python.org/pypi/pymeshio/',
         license='zlib',
-        #package_dir={
-        #    'pymeshio': 'blender25-meshio/pymeshio'
-        #    },
-        packages=[
-            'pymeshio',
-            'pymeshio.mqo',
-            'pymeshio.pmd',
-            'pymeshio.pmx',
-            'pymeshio.vmd',
-            ],
+        packages=find_packages(),
         test_suite='nose.collector',
         tests_require=['Nose'],
         zip_safe = (sys.version>="2.5"),   # <2.5 needs unzipped for -m to work
