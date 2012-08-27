9885d552715bfe37fb5db4de78206586753af47d
ousttrue <ousttrue@gmail.com>
2012-08-28 02:31:22 +0900

    add 3.0.0 stub

---
+++ pymeshio/blender/__init__.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+import bpy
+
+
+def import_file(filepath, *args):
+    print(args)
+
+
+def export_pmd(filepath, *args):
+    print('export_pmd', args)
+
+def export_pmx(filepath, *args):
+    print('export_pmx', args)
+
+def export_mqo(filepath, *args):
+    print('export_mqo', args)
+
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import shutil
 
 
 # copy pymeshio dir for blender26 plugin
-PYMESHIO_DIR_IN_BLENDER26='blender26-meshio/pymeshio'
+PYMESHIO_DIR_IN_BLENDER26='blender26-meshio2/pymeshio'
 if os.path.exists(PYMESHIO_DIR_IN_BLENDER26):
     shutil.rmtree(PYMESHIO_DIR_IN_BLENDER26)    
 print("copy pymeshio to %s" % PYMESHIO_DIR_IN_BLENDER26)
@@ -17,7 +17,7 @@ shutil.copytree('pymeshio', PYMESHIO_DIR_IN_BLENDER26)
 
 setup(
         name='pymeshio',
-        version='2.6.5',
+        version='3.0.0',
         description='3d model io library for mqo, pmd, pmx, vmd and vpd',
         long_description=open('README.rst').read(),
         classifiers=[
