153a89f111707322cb0a1e581a1fb8369fb26774
ousttrue <ousttrue@gmail.com>
2012-08-28 14:55:38 +0900

    implement read pmx vertices

--- pymeshio/util.py
+++ pymeshio/util.py
@@ -2,10 +2,88 @@
 import sys
 import os
 
+from . import pmx
+import pmx.reader
+
+
+class Material(object):
+    __slots__=[
+            'name',
+            ]
+    def __init__(self, name):
+        self.name=name
+
+
+class Vector2(object):
+    __slots__=[
+            'x', 'y',
+            ]
+    def __init__(self, x, y):
+        self.x=x
+        self.y=y
+
+
+class Vector3(object):
+    __slots__=[
+            'x', 'y', 'z'
+            ]
+    def __init__(self, x, y, z):
+        self.x=x
+        self.y=y
+        self.z=z
+
+
+class Vertex(object):
+    __slots__=[
+            'pos',
+            'normal',
+            'uv',
+            'skinning',
+            ]
+    def __init__(self, pos, uv, normal=None, skinning=None):
+        self.pos=pos
+        self.uv=uv
+        self.normal=normal
+        self.skinning=skinning
+
+
+class Mesh(object):
+    __slots__=[
+            'vertices', 'faces',
+            ]
+    def __init__(self):
+        pass
+
 
 class GenericModel(object):
+    __slots__=[
+            'meshes',
+            ]
+
     def __init__(self):
-        super(GenericModel, self).__init__()
+        self.meshes=[]
+
+    def load_pmx(self, src):
+        mesh=Mesh()
+        def parse_pmx_vertex(v):
+            if isinstance(v.deform, pmx.Bdef1):
+                skinning=[(v.deform.index0, 1.0)]
+            elif isinstance(v.deform, pmx.Bdef2):
+                skinning=[
+                        (v.deform.index0, v.deform.weight0), 
+                        (v.deform.index1, 1.0-v.deform.weight0)
+                        ]
+            else:
+                raise 'unknown deform !'
+            vertex=Vertex(
+                    pos=Vector3(v.position.x, v.position.y, v.position.z),
+                    uv=Vector2(v.uv.x, v.uv.y),
+                    normal=Vector3(v.normal.x, v.normal.y, v.normal.z),
+                    skinning=skinning)
+            return vertex
+        mesh.vertices=[parse_pmx_vertex(v) for v in src.vertices]
+        self.meshes.append(mesh)
+
 
     @staticmethod
     def read_from_file(filepath):
@@ -17,16 +95,20 @@ class GenericModel(object):
 
         model=GenericModel()
         if ext==".pmd":
-            from .pmd import reader
-            m=reader.read_from_file(filepath)
+            from . import pmd
+            import pmd.reader
+            m=pmd.reader.read_from_file(filepath)
 
         elif ext==".pmx":
-            from .pmx import reader
-            m=reader.read_from_file(filepath)
+            m=pmx.reader.read_from_file(filepath)
+            if not m:
+                return
+            model.load_pmx(m)
 
         elif ext==".mqo":
-            from .mqo import reader
-            m=reader.read_from_file(filepath)
+            from . import mqo
+            import mqo.reader
+            m=mqo.reader.read_from_file(filepath)
 
         else:
             print('unknown file type: '+ext)
--- test/util_test.py
+++ test/util_test.py
@@ -3,6 +3,7 @@ import unittest
 import io
 import pymeshio.util
 import pymeshio.common
+import pymeshio.pmx.reader
 
 
 PMX_FILE=pymeshio.common.unicode('resources/初音ミクVer2.pmx')
@@ -14,8 +15,10 @@ class TestUtil(unittest.TestCase):
         pass
 
     def test_read(self):
+        src=pymeshio.pmx.reader.read_from_file(PMX_FILE)
         model=pymeshio.util.GenericModel.read_from_file(PMX_FILE)
         self.assertEqual(pymeshio.util.GenericModel,  model.__class__)
+        self.assertEqual(len(src.vertices), len(model.meshes[0].vertices))
 
     def test_write(self):
         # read source file
