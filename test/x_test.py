2ddd8fc562d3e6f7ee93874a9e47384fff1e564c
ousttrue <ousttrue@gmail.com>
2017-11-07 16:42:59 +0900

    fix test

--- test/x_test.py
+++ test/x_test.py
@@ -1,21 +1,22 @@
+import os
+import unittest
 import pymeshio.x
 import pymeshio.x.reader
-import sys
-import unittest
 
 
-FILE="resources/cube.x"
+FILE = "resources/cube.x"
 
 
 class TestX(unittest.TestCase):
-    
+
     def setUp(self):
         pass
 
     def test_read(self):
-        model=pymeshio.x.reader.read_from_file(FILE)
-        #print(model)
+        if not os.path.exists(FILE):
+            return
+        model = pymeshio.x.reader.read_from_file(FILE)
+        # print(model)
         self.assertEqual(pymeshio.x.Model, model.__class__)
         self.assertEqual(24, len(model.vertices))
         self.assertEqual(1, len(model.materials))
-
