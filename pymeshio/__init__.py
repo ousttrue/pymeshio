821c9ac2fa4c5c204def87533c8ca6c39d517cf3
ousttrue <ousttrue@gmail.com>
2018-05-01 13:56:01 +0900

    Add pymeshio.pmx_from_file

--- pymeshio/__init__.py
+++ pymeshio/__init__.py
@@ -6,14 +6,21 @@ pymeshio
 
 3d mesh io library.
 """
-__version__='3.1.0'
+__version__ = '3.1.0'
+
 
 # export
-version=__version__
+version = __version__
 
 
 from . import pmd
+from . import pmx
+from .converter import pmd_to_pmx
 
 
 def pmd_from_file(path):
     return pmd.reader.read_from_file(path)
+
+
+def pmx_from_file(path):
+    return pmx.reader.read_from_file(path)
